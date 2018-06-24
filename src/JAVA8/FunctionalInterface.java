390b8679079d7de61b00c32cfbc281360e702b54
sahilk335 <sahilk335@gmail.com>
2018-06-24 15:56:42 +0530

    FunctionalInterface Readme

--- src/JAVA8/FunctionalInterface.java
+++ src/JAVA8/FunctionalInterface.java
@@ -2,5 +2,20 @@ package JAVA8;
 
 @java.lang.FunctionalInterface
 public interface FunctionalInterface {
+
+
     public void add();
+
+    /*
+    Static methods can be there in functional interface
+    Note : Static methods of interface can be only accessed with interface name
+    like print can be accessed like.. FunctionalInterface.print();
+    */
+    public static void print(){
+        System.out.print("Static Method inside Functional Interface");
+    }
+
+    public default void printDefault(){
+        System.out.print("Default Method inside Functional Interface");
+    }
 }
---
+++ src/JAVA8/README.md
@@ -0,0 +1,7 @@
+# 1. Functional Interfaces
+
+1. It must contain only 1 abstract methods
+2. It can contain any number of default or static methods
+3. Annotation @FunctionalInterface can be given to make sure it is functional interface
+4. There are many default functional interfaces that are provided by Java 8 , example - 
+	Predicate,BiPredicate,Supplier,Functional,BiFunctional,Unaryoperator
